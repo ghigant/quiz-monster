863d2bbe6c1372a815d1ac0442f0db714398ae46
Valentin Botoc <ghigant@gmail.com>
2019-01-13 17:28:55 +0200

    unused variables

--- client/src/store/epics/login.js
+++ client/src/store/epics/login.js
@@ -1,7 +1,6 @@
 import {combineEpics, ofType} from 'redux-observable';
 import {of} from 'rxjs';
-import {switchMap, concat, catchError, mergeMap, map } from 'rxjs/operators';
-import { ajax } from 'rxjs/ajax';
+import {switchMap, concat, map } from 'rxjs/operators';
 
 import {LOGIN, LOGOUT, setProgressState} from '../reducers/authentication';
 
